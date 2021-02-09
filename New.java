9d6973a41eaabdcc0f64a7f498c9afabfeb6d49e
jesse6337 <69617936+jesse6337@users.noreply.github.com>
2021-02-08 21:05:48 -0500

    Add files via upload

---
+++ New.java
@@ -0,0 +1,10 @@
+
+public class New {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i = 0; i <= 100000000; i++) {
+            System.out.println(i);
+            System.out.println("fuck it");
+        }
+    }
+}
